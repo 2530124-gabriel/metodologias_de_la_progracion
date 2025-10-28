181695379bee772842531a0d8a05778ac6eec06e
Gabriel Nuñez <gabogahi@gmail.com>
2025-10-28 10:39:51 -0600

    2530124@upv.edu.mx

---
+++ src/understanding_strings.py
@@ -0,0 +1,14 @@
+"""
+    String variables
+    
+    Un string es de manera sencilla una seria
+    de carcteres. En python, todo lo que se encuentre dentro
+    de cinukkas (' ') o dobles (" ") 
+    se considerado un string.
+
+    Ejemplos:
+        "Esto es un string"
+        'Esto tambien es un string'
+        'Le dije a un amigo "Python es mi lenguaje favorito" '
+        "El lenguaje 'Python lleva el nombre por Monty Python, no por la serpiente' "
+"""
