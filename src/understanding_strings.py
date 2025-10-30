94ee622b278dd1fa41a69cf758d8634683a39ccd
Gabriel Nuñez <gabogahi@gmail.com>
2025-10-30 10:40:55 -0600

    sintaxiserror

--- src/understanding_strings.py
+++ src/understanding_strings.py
@@ -33,10 +33,40 @@ En esta ocacion el metodo .title no requiere informacion adicional para funciona
 """
 print("Metodo upper: ", name.upper())
 print("Metodo lower: ", name.lower())
-
+    
 # Concatenacion de STRINGS 
 first_name= "charly"
 last_name= "mercury"
 full_name= first_name +" "+ last_name
 print(full_name)
 print(full_name.title())
+
+# Whitesspaces
+"""
+    Un whitespace se refiere a cualquier caracter que no se imprime, es decir,
+    un espacio, tabuladores y finales de linea. Los whitespaces se utilizan comunmente 
+    para organizar las salidas de tal manera que sea mas amigable de leer o ver para el usuario.
+
+    Ejermplo:
+        - Tabulador: \t
+        - Salto de linea: \n
+"""
+print("Python")
+print("\tPython")
+print("\t\tPython")
+
+print("Whitespace es salto de linea")
+print("Languagues: \n\tPython\nC\n\tJavasscript")
+
+# Eliminacion de espacios en blanco
+programming_languages= " Python "
+print(programming_languages)
+print(programming_languages.lstrip())
+print(programming_languages.rstrip())
+print(programming_languages.strip())
+
+# Synta Error con String
+message= 'Una fortaleza de python es su comunidad'
+print(message)
+message= Una fortaleza de python es su comunidad'
+print(mesage)
