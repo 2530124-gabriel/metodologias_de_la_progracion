ef35f0627a74ba28b38e3b386059d484b068fd9d
Gabriel Nuñez <gabogahi@gmail.com>
2025-11-20 10:36:58 -0600

    diccionario

---
+++ src/understanding_dicotionaries.py
@@ -0,0 +1,28 @@
+# Empty Dictionary
+homer_0= {
+    "color": "yellow", 
+    'bag': "maggie-bag", 
+    "hair": "black", 
+    "dress": "casual", 
+    "mom": "false"}
+#print (homer_0)
+#print(type(homer_0))
+
+marge= {
+    "color": "yellow", 
+    'bag': "homer-donit", 
+    "hair": "blue", 
+    "dress": "informal", 
+    "mom": "true"}
+gun_0 = {"scar": "yellow-orange", "headshot": 1.5}
+
+## Add elements to a dictionary
+print(homer_0)
+homer_0["x-position"]=15
+homer_0["y-position"]=25
+homer_0["z-position"]=10
+print(homer_0)
+
+marge["x-position"]=16
+marge["y-position"]=26
+marge["z-position"]=10
--- src/understanding_if_statement_2.py
+++ src/understanding_if_statement_2.py
@@ -15,7 +15,29 @@ elif age >= 0 and age < 18:
         print("Eres menor de edad")
 else:
     print("Tuviste un error")
+#
+try: 
+    age= int(input("¿Cual es tu edad?: "))
+except:
+    age= -1 
+if age<= 4 and age >=0 :
+    print("Tu entrada es gratuita")
+elif age<=17 and age >=5 : 
+        print("Deberas pagar $200")
+elif age>=18:       
+        print("Deberas pagar $400")
+else:
+    print("Error, ingresaste un caracter no valido")
 
+# Utilizando varias listas
+guisos_disponibles = ["salsa verde", "deshebrada", "mole"]
+guisos_a_ordenar= ["deshebrada", "caldo de iguana"]
 
-
-
+print("¿Que guisos deseas ordenar?")
+for guiso in guisos_a_ordenar:
+    print(f"Deseo {guiso}")
+    if guiso in guisos_disponibles:
+        print(f"Si tenemos {guiso}")
+    else:
+        print(f"Lo siento, no tenemos {guiso}")
+print("Realizando pedido....")
