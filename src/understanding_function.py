0b85624fae9191d72dd0da474c7e73c74dde9b4e
Gabriel Nuñez <gabogahi@gmail.com>
2025-11-27 10:32:25 -0600

    function

---
+++ src/understanding_function.py
@@ -0,0 +1,24 @@
+### Funciones
+# Las funciones son bloques de codigo para realizar una tarea en especifico
+# Cuando queremos realiar la tarea que sea definido en la funcion, tenemos que llamar
+# el nombre de la funcion que realiza la accion.
+
+"""
+    Sintaxis de una funcion
+    def nombre funcion():
+        acciones
+
+
+
+
+        Ejemplo: vamos a definir una funcion de un saludo
+        a Christopher
+"""
+def gretting_christopher():
+    """
+        Funcion para saludar a una persona llamada
+        Christopher
+    """
+    for i in range (0,5):
+        print("Hello Christopher")
+gretting_christopher()
